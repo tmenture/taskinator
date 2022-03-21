edd7e88f46247fff45ab1c783c4f9002c474d2d8
tmenture <thomasoxemail@gmail.com>
2022-03-21 14:04:55 -0400

    button now stores and presents options typed in text area and prints selected type

--- assets/js/taskinator.js
+++ assets/js/taskinator.js
@@ -2,15 +2,21 @@ var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
 
-    console.log(event);
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+
+    tasksToDoE1.appendChild(listItemE1);
 };
 
 formE1.addEventListener("submit", createTaskHandler);
