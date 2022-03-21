35483cc55e1dfc6df99351b731c61781aa5319e8
tmenture <thomasoxemail@gmail.com>
2022-03-21 15:01:48 -0400

    future proofed the code with some minor refactoring

--- assets/js/taskinator.js
+++ assets/js/taskinator.js
@@ -1,22 +1,31 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
 
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
 
     var taskInfoE1 = document.createElement("div");
     taskInfoE1.className = "task-info";
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
-    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     listItemE1.appendChild(taskInfoE1);
 
     tasksToDoE1.appendChild(listItemE1);
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+formE1.addEventListener("submit", taskFormHandler);
